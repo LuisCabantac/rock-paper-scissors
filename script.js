de1b93d6cc0bc89fa036230e042b70d0c0e6316d
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-06 06:45:35 +0800

    Assigned the values of each cases with the variable computerChoice

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
 
-    let ComputerChoice;
+    let computerChoice;
     switch (randomNumber) {
         case 0:
-            console.log("Rock");
+            computerChoice = "Rock";
             break;
         case 1:
-            console.log("Paper");
+            computerChoice = "Paper";
             break;
         case 2:
-            console.log("Scissors");
+            computerChoice = "Scissors";
             break;
     }
-    return ComputerChoice;
+    return computerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+    
+    if (player === computer) {
+        return "It's a tie!";
+    }
+    
+    if (
+        (player === 'rock' && computer === 'scissors') ||
+        (player === 'paper' && computer === 'rock') ||
+        (player === 'scissors' && computer === 'paper')
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
 }
