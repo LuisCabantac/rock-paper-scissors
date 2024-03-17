775b4d4a9e1af27765dc1e6a2dfb7694504bc5eb
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-18 04:52:51 +0800

    Implemented scoring system to the game

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
-const playerSelection = prompt("Rock, Paper, Scissors");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+const emojiIcons = document.querySelector("#result");
+const playerSelection = document.querySelectorAll(".btn-selection");
+let selection = "";
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
 
-function playGame() {
-    getComputerChoice()
-    playRound(playerSelection, computerSelection);
-}
+playerSelection.forEach(el =>{
+  el.addEventListener("click", function() {
+    selection = this.value;
+    const computerSelection = getComputerChoice();;
+    playRound(selection, computerSelection);
+
+    const result = playRound(selection, computerSelection);
+    if (result === 'win') {
+      playerScore++;
+    } else if (result === "tie") {
+      tieScore++;
+    } else {
+      computerScore++;
+    }
+    updateScore();
+  });
+});
 
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3);
+  const randomNumber = Math.floor(Math.random() * 3);
 
-    let computerChoice;
-    switch (randomNumber) {
-        case 0:
-            computerChoice = "Rock";
-            break;
-        case 1:
-            computerChoice = "Paper";
-            break;
-        case 2:
-            computerChoice = "Scissors";
-            break;
-    }
-    return computerChoice;
+  let computerChoice;
+  switch (randomNumber) {
+    case 0:
+      computerChoice = "rock";
+      break;
+    case 1:
+      computerChoice = "paper";
+      break;
+    case 2:
+      computerChoice = "scissors";
+      break;
+  }
+  return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    const player = playerSelection.toLowerCase();
-    const computer = computerSelection.toLowerCase();
-    
-    if (player === computer) {
-        return "It's a tie!";
-    }
-    
-    if (
-        (player === 'rock' && computer === 'scissors') ||
-        (player === 'paper' && computer === 'rock') ||
-        (player === 'scissors' && computer === 'paper')
-    ) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    }
-    else if (player !== "rock" && player !== "paper" && player !== "scissors") {
-        return "Invalid selection"
-    }
-    else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
+function playRound(selection, computerSelection) {
+  let player = selection;
+  let computer = computerSelection;
+  if (player === computer ) {
+    emojiIcons.textContent = "It's a tie!";
+    return "tie";
+  }
+
+  if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")
+  ) {
+    emojiIcons.textContent = `You Win! ${selection} beats ${computerSelection}`;
+    return "win"
+  } else {
+    emojiIcons.textContent = `You Lose! ${computerSelection} beats ${selection}`;
+    return "lose"
+  }
 }
 
-playGame()
+function updateScore() {
+  document.querySelector("#won-text").textContent = playerScore;
+  document.querySelector("#lose-text").textContent = computerScore;
+  document.querySelector("#draw-text").textContent = tieScore;
+}
